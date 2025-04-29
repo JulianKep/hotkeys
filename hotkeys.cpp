b2147e1c868d3241be7a28a8bf336eac09ac988e
JulianKep <132519348+JulianKep@users.noreply.github.com>
2025-04-29 23:06:29 +0200

    29.04.2025_23_06_28,84

--- hotkeys.cpp
+++ hotkeys.cpp
@@ -1,33 +1,63 @@
 #include <windows.h>
-#include <stdio.h>
+#include <iostream>
 
-LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
-    if (nCode == HC_ACTION && wParam == WM_KEYDOWN) {
-        KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*)lParam;
+HHOOK hHook = NULL;
 
-        if (p->vkCode == 'P') {
-            printf("P key pressed!\n");
+
+//LRESULT is a long in Windows
+//CALLBACK is a calling convention
+//WPARAM unsigned integer
+//LPARAM long pointer sized value
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+    if (nCode == HC_ACTION)
+    {
+        KBDLLHOOKSTRUCT* pKeyboard = (KBDLLHOOKSTRUCT*)lParam;
+        
+        if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)
+        {
+            // Check if Ctrl + Alt + H is pressed
+            bool ctrlDown = (GetAsyncKeyState(VK_CONTROL) & 0x8000) != 0;
+            bool altDown = (GetAsyncKeyState(VK_MENU) & 0x8000) != 0;
+
+            if (ctrlDown && altDown && pKeyboard->vkCode == 'H')
+            {
+                std::cout << "Hotkey Ctrl+Alt+H pressed!" << std::endl;
+            }
         }
     }
 
-    return CallNextHookEx(NULL, nCode, wParam, lParam);
+    return CallNextHookEx(hHook, nCode, wParam, lParam);
 }
 
-int main() {
-    HHOOK hook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, NULL, 0);
-    if (!hook) {
-        printf("Failed to install hook.\n");
+
+
+
+int main()
+{
+    // Set the hook
+    hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
+
+    if (hHook == NULL)
+    {
+        std::cout << "Failed to install hook!" << std::endl;
         return 1;
     }
+    else
+    {
+        std::cout << "Hook installed. Press Ctrl+Alt+H..." << std::endl;
+    }
 
-    printf("Listening for P key...\n");
-
+    // Message loop to keep the application running
     MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
-    UnhookWindowsHookEx(hook);
+    // Unhook before exit
+    UnhookWindowsHookEx(hHook);
+
     return 0;
 }
