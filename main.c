b23fee6f5924c9e0d7e562c68d3c9c6c9e1bd48b
JulianKep <132519348+JulianKep@users.noreply.github.com>
2025-05-01 13:41:55 +0200

    01.05.2025_13_41_55,36

--- main.c
+++ main.c
@@ -3,9 +3,18 @@
 #include <stdlib.h>
 
 int main() {
-    // Example: Register Ctrl + Alt + H (Virtual-Key 0x48)
+    //the function inside the if clause is executed using the parameters, it returns 0 if it executed succesfully
+    //NULL beceause there is no window id that the message is sent back to
+    //1 is an id to reference this hotkey inside the programm
     if (!RegisterHotKey(NULL, 1, MOD_CONTROL | MOD_ALT, 0x48)) {
         printf("Failed to register hotkey. Error code: %d\n", GetLastError());
+        //pause to display error message
+        getchar();
+        return 1;
+    }
+    if (!RegisterHotKey(NULL, 2, MOD_CONTROL | MOD_ALT, 0x4E)) {
+        printf("Failed to register hotkey. Error code: %d\n", GetLastError());
+        //pause to display error message
         getchar();
         return 1;
     }
@@ -13,11 +22,18 @@ int main() {
     printf("Hotkey registered. Press Ctrl+Alt+H...\n");
 
     MSG msg = {0};
+    //gebe pointer von msg
     while (GetMessage(&msg, NULL, 0, 0)) {
+        //is the received message of type hotkey
         if (msg.message == WM_HOTKEY) {
+            //is the id of the received hotkey 1?
             if (msg.wParam == 1) {
                 printf("Hotkey pressed!\n");
+
                 ShellExecute(NULL, "open", "http://homeassistant.local:8123/config/hardware", NULL, NULL, SW_SHOWNORMAL);
+            }
+            if (msg.wParam == 2) {
+                ShellExecute(NULL, "open", "notepad.exe", NULL, NULL, SW_SHOWNORMAL);
             }
         }
     }
