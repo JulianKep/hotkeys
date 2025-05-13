b6edc252ea9a2946f606491c72a3e8343cb7903f
JulianKep <132519348+JulianKep@users.noreply.github.com>
2025-05-13 15:52:01 +0200

    13.05.2025_15_52_01,25

--- Readme.md
+++ Readme.md
@@ -4,4 +4,10 @@
 
 - [ ] config hotkeys by using a json
 
+- [ ] compile json library
 
+- [ ] integrate calendar in programm
+
+- [ ] make it so it shows stuff in special pixelcoordiantes
+
+- [ ] when executing a shellexecute(start, notepad) in c can you make i t so the notepad appears in a certain location on the secdonscreen
--- main.c
+++ main.c
@@ -4,11 +4,77 @@
 
 #define LONG_LIVED_ACCESS_TOKEN = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI3ZDAwNGRiMWRjZTU0Yzg1YmIzM2JiMTFmZTkyMGM4YSIsImlhdCI6MTc0NjExNzI1MiwiZXhwIjoyMDYxNDc3MjUyfQ.K_OI7EAvliHJMx6TA_9FRudMzZkcxZQeRs523KROoH0
 
+typedef struct
+{
+    char first_key[8];
+    char second_key[8];
+    char third_key;
+    char command[10];
+    char modifier[300];
+
+} Hotkey;
+
 int main()
 {
-    // the function inside the if clause is executed using the parameters, it returns 0 if it executed succesfully
-    // NULL beceause there is no window id that the message is sent back to
-    // 1 is an id to reference this hotkey inside the programm
+
+    FILE *file;
+    // fopen returns a pointer to  a file
+    file = fopen("config.txt", "r");
+
+    if (file == NULL)
+    {
+        printf("Error opening file");
+        return 1;
+    }
+
+    Hotkey hotkeys[100];
+
+    // used to verify that the correct number of values where read from each line
+    int read = 0;
+
+    // keeps track of number of hotkey combinations
+    int combo_number = 0;
+
+    do
+    {
+        // fscanf returns numbers of read values
+        read = fscanf(file, "%8[^,],%8[^,],%c,%10[^,],%100[^\n]",
+                      hotkeys[combo_number].first_key,
+                      hotkeys[combo_number].second_key,
+                      &hotkeys[combo_number].third_key,
+                      hotkeys[combo_number].command,
+                      hotkeys[combo_number].modifier);
+
+        if (read == 5)
+        {
+            combo_number++;
+        }
+        if (read != 5 && !feof(file))
+        {
+            printf("File formatting issue");
+            return 1;
+        }
+
+        // feof returns true if end of file is reached
+    } while (!feof(file));
+
+    fclose(file);
+    
+    
+    for (int i=0; i < combo_number; i++){
+        printf("%s, %s, %c, %s, %s\n", 
+            hotkeys[i].first_key,
+            hotkeys[i].second_key,
+            hotkeys[i].third_key,
+            hotkeys[i].command,
+            hotkeys[i].modifier
+        );
+    }
+    getchar();
+
+    /* the function inside the if clause is executed using the parameters, it returns 0 if it executed succesfully
+    NULL beceause there is no window id that the message is sent back to
+    1 is an id to reference this hotkey inside the programm */
     if (!RegisterHotKey(NULL, 1, MOD_CONTROL | MOD_ALT, 0x48))
     {
         printf("Failed to register hotkey. Error code: %d\n", GetLastError());
@@ -24,6 +90,13 @@ int main()
         return 1;
     }
     if (!RegisterHotKey(NULL, 3, MOD_CONTROL | MOD_ALT, 0x4C))
+    {
+        printf("Failed to register hotkey. Error code: %d\n", GetLastError());
+        // pause to display error message
+        getchar();
+        return 1;
+    }
+    if (!RegisterHotKey(NULL, 4, MOD_CONTROL | MOD_ALT, 0x50))
     {
         printf("Failed to register hotkey. Error code: %d\n", GetLastError());
         // pause to display error message
@@ -49,6 +122,9 @@ int main()
             case 3:
                 system("curl -X POST -H \"Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI3ZDAwNGRiMWRjZTU0Yzg1YmIzM2JiMTFmZTkyMGM4YSIsImlhdCI6MTc0NjExNzI1MiwiZXhwIjoyMDYxNDc3MjUyfQ.K_OI7EAvliHJMx6TA_9FRudMzZkcxZQeRs523KROoH0\" http://homeassistant.local:8123/api/webhook/-6QDwsBUTH2duVkWPiaNwfdEh");
                 break;
+            case 4:
+                ShellExecute(NULL, "open", "notepad.exe", NULL, NULL, SW_SHOWNORMAL);
+                break;
             }
         }
     }
@@ -56,6 +132,7 @@ int main()
     UnregisterHotKey(NULL, 1);
     UnregisterHotKey(NULL, 2);
     UnregisterHotKey(NULL, 3);
+    UnregisterHotKey(NULL, 4);
     return 0;
 }
 
